5fe0cbcaa747476fdf3446d46b9efa5f8d69b26c
fluxspirix <147746395+fluxspirix@users.noreply.github.com>
2024-02-16 21:30:20 -0600

    Update x.py

--- x.py
+++ x.py
@@ -1,5 +1,5 @@
 def say_name(namx):
     print(f"Hi there! {namx}. How are you today?")
 
-nombre = input("input your name: ").capitalize()
+nombre = input("Digit your name: ").capitalize()
 say_name(nombre)
