3e978d3822879de872bac068af490d1299012d67
dejavu x <elxnrxmrx@gmail.com>
2024-02-16 21:16:50 -0600

    modify one line

--- s.py
+++ s.py
@@ -2,4 +2,4 @@ namx = "Omar"
 lastname = "Romero"
 networth = "900.000.000.000.00"
 
-print(f"my name {namx} {lastname} and my fortune is {networth}")
+print(f"my name is {namx} {lastname} and my fortune is {networth}")
